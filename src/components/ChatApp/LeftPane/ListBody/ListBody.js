25a179efeb0f8c8e91d9628e98040d51c835f267
Alex Wilson <acwilson96@protonmail.com>
2017-09-26 02:30:37 +0100

    Merge latest updates to UI to POC.

--- src/components/ChatApp/LeftPane/ListBody/ListBody.js
+++ src/components/ChatApp/LeftPane/ListBody/ListBody.js
@@ -13,9 +13,10 @@ export default class ListBody extends Component {
     }
     
     render() {
-
+        var chats = this.props.chats;
+        chats = chats.sort(function(a,b) {return (a.updatedAt < b.updatedAt) ? 1 : ((b.updatedAt > a.updatedAt) ? -1 : 0);} );
         // Create a list of FriendEntrys.
-        const chatList = this.props.chats.map((chat) =>
+        const chatList = chats.map((chat) =>
             // Create friend entry, pass data and remove method down.
             <ChatEntry io={this.props.io} data={chat} key={chat.id} openChat={this.props.openChat} user={this.props.user} />
         );
